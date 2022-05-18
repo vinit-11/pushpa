5e73bfd7e9dfcf169a9b0dc04f1b87977b93c767
vinit-11 <105799066+vinit-11@users.noreply.github.com>
2022-05-18 19:33:46 +0530

    Add files via upload

---
+++ TestBase.zip.java
@@ -0,0 +1,31 @@
+package base.testvagrant;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+
+
+	public static WebDriver driver;
+	
+
+	public  static void initialization() {				//static
+		
+		
+		WebDriverManager.chromedriver().setup();
+
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+	}
+
+}
